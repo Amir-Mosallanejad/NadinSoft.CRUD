324ec9e46161d50fbc2a773a2bc9a2408983508e
amir-mosallanejad <mosallanejad.amir@gmail.com>
2025-07-13 19:11:32 +0330

    Add BaseEntity And Product Entity

---
+++ src/NadinSoft.CRUD.Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace NadinSoft.CRUD.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    public DateTime ProduceDate { get; set; }
+    public string ManufacturePhone { get; set; } = null!;
+    public string ManufactureEmail { get; set; } = null!;
+    public bool IsAvailable { get; set; }
+}
