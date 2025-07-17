f25893975a50995691cf51f3c7cb741a437674cd
amir-mosallanejad <mosallanejad.amir@gmail.com>
2025-07-17 13:50:41 +0330

    Add Example to PhoneNumber Validator

--- src/NadinSoft.CRUD.Application/Services/ProductService/Command/CreateProduct/CreateProductRequestValidator.cs
+++ src/NadinSoft.CRUD.Application/Services/ProductService/Command/CreateProduct/CreateProductRequestValidator.cs
@@ -16,7 +16,7 @@ public class CreateProductRequestValidator : AbstractValidator<CreateProductRequ
         RuleFor(x => x.Dto.ManufacturePhone)
             .MaximumLength(20).WithMessage("phone number cannot exceed 20 characters.")
             .NotEmpty().WithMessage("Manufacture phone is required.")
-            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Invalid phone number format.");
+            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Invalid phone number format. Example: +98901.......");
 
         RuleFor(x => x.Dto.ManufactureEmail)
             .MaximumLength(100).WithMessage("Manufacture email cannot exceed 100 characters.")
--- src/NadinSoft.CRUD.Application/Services/ProductService/Command/UpdateProduct/UpdateProductRequestValidator.cs
+++ src/NadinSoft.CRUD.Application/Services/ProductService/Command/UpdateProduct/UpdateProductRequestValidator.cs
@@ -19,7 +19,7 @@ public class UpdateProductRequestValidator : AbstractValidator<UpdateProductRequ
         RuleFor(x => x.Dto.ManufacturePhone)
             .MaximumLength(20).WithMessage("phone number cannot exceed 20 characters.")
             .NotEmpty().WithMessage("Manufacture phone is required.")
-            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Invalid phone number format.");
+            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Invalid phone number format. Example: +98901.......");
 
         RuleFor(x => x.Dto.ManufactureEmail)
             .MaximumLength(100).WithMessage("Manufacture email cannot exceed 100 characters.")
